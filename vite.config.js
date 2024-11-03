a70b7496b36c2d674f4910e99fcc0ee628196af6
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-03 23:50:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import markdown from 'vite-plugin-markdown';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    markdown() // Add the markdown plugin here
+  ],
+  assetsInclude: ['**/*.md'] // Include markdown files as assets if needed
+});
